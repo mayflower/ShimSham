0fc7ec8093e6173615f66d049d91c0de76774c05
micha trieba <micha.trieba@mayflower.de>
2014-11-12 21:12:15 +0100

    changed date to string, added weekday pull down

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112151739) do
+ActiveRecord::Schema.define(version: 20141112173053) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20141112151739) do
   create_table "dance_classes", force: true do |t|
     t.string   "name"
     t.integer  "level_id"
-    t.date     "day"
+    t.string   "day"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
