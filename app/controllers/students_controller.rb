fb4fc926cbfb09fea1933f59d18a0291cdf96eb2
micha trieba <micha.trieba@mayflower.de>
2014-11-22 14:41:11 +0100

    removed broken relations

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -69,6 +69,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:name, :street, :zip, :city, :phone, :country, :groups_id, :DanceClasses_id, :email)
+      params.require(:student).permit(:name, :street, :zip, :city, :phone, :country, :groups_id, :email)
     end
 end
