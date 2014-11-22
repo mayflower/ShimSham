4ee114b44f9dfebda72e9fb606d3dd1aa56f1265
micha trieba <micha.trieba@mayflower.de>
2014-11-22 11:06:20 +0100

    removed typo in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = falserender plain: params[:article].inspect
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
