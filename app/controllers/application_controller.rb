3432be50cecb5da69e923376845fd73739b254b6
Florian Fackler <florian.fackler@mintao.com>
2014-12-05 15:32:02 +0100

    Show admin menu only if user has admin role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   protect_from_forgery with: :exception
+
+  before_action :assign_user
+
+  def assign_user
+    @user = User.new
+  end
 end
