923162f599b5f4fc8651f4a0c08f2e1c885608c8
micha trieba <micha.trieba@mayflower.de>
2015-05-24 23:47:53 +0200

    fixed references

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
   has_and_belongs_to_many :lessons
-  has_and_belongs_to_many :unit
+  has_and_belongs_to_many :units
   has_and_belongs_to_many :dance_classes
 end
